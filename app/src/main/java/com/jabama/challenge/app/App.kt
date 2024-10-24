da1570c263f2f247ae8d2bf4df6b326474a79c9a
Melika Afrakhteh <melikaafrakhteh@gmail.com>
2024-10-24 13:02:58 +0330

    Replace better import file address for network modules

--- app/src/main/java/com/jabama/challenge/app/App.kt
+++ app/src/main/java/com/jabama/challenge/app/App.kt
@@ -2,9 +2,9 @@ package com.jabama.challenge.app
 
 import android.app.Application
 import androidx.preference.PreferenceManager
-import com.jabama.challenge.network.di.accessTokenModule
-import com.jabama.challenge.network.di.networkModule
 import com.jabama.challenge.repository.token.TokenRepositoryImpl
+import com.jabama.network.di.accessTokenModule
+import com.jabama.network.di.networkModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.qualifier.named
@@ -16,7 +16,13 @@ class App : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            modules(listOf(appModule, networkModule, accessTokenModule))
+            modules(
+                listOf(
+                    appModule,
+                    networkModule,
+                    accessTokenModule
+                )
+            )
         }
     }
 
