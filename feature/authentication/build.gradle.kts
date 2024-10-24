bf688945b5daab3fd672e14593fc34fcd2f3ccea
Melika Afrakhteh <melikaafrakhteh@gmail.com>
2024-10-24 11:54:07 +0330

    Create authentication module in feature

---
+++ feature/authentication/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.jabama.authentication"
+    compileSdk = 34
+}
+
+dependencies {}
