b42e031448ad105533b85a2502ca48a941d24ff0
Melika Afrakhteh <melikaafrakhteh@gmail.com>
2024-10-24 12:31:52 +0330

    Create network module in core

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.jabama.core.network"
+    compileSdk = 34
+}
+
+dependencies {}
